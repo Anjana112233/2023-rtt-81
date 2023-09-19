a5827b152c00a93915d601bfc546f4af5ddda369
Anjana111222 <karkianjana111@gmail.com>
2023-09-19 09:11:44 -0500

    My first variable pratice

--- Secondproject/src/secondfile/SecondClass.java
+++ Secondproject/src/secondfile/SecondClass.java
@@ -2,7 +2,17 @@ package secondfile;
 
 public class SecondClass {
 	public static void main(String[] args) {
+		double radius;
+		double area;
+		//assign radius
+		radius = 20;
+		area = radius * radius * 3.1459;
+		
+		//Display results
+		System.out.println("The area of the circle of radius " + radius + "is" + area);
 		System.out.println("Hello world second time");
 	}
 
 }
+
+
