90bd658ed0bb00d3d1f3d5d998e33711b9a31e1e
Anjana111222 <karkianjana111@gmail.com>
2023-09-22 13:52:15 -0500

    pratice 303.4.1 conditional

---
+++ Secondproject/src/secondfile/ConditionalPratice2.java
@@ -0,0 +1,13 @@
+package secondfile;
+
+public class ConditionalPratice2 {
+	public static void main (String[] args) {
+		int x = 5;
+		if ((x < 10) || (x > 20)){
+			System.out.println("Out of range");
+			}else if ((x <= 10) && (x >= 20)) {
+			System.out.println("In range");
+		}
+	}
+
+}
