832410ff82892b0b886a3754d71daac95a4b26b3
Anjana111222 <karkianjana111@gmail.com>
2023-09-22 14:56:03 -0500

    Glab 303.3.1 - example 4 tolowercase

---
+++ ThirdprojectJava/src/stringmethods/StringLowerExample.java
@@ -0,0 +1,11 @@
+package stringmethods;
+
+public class StringLowerExample {
+	public static void main(String[] args) {
+		String s1 = ("HELLO HOw are you?");
+		String s1lower=s1.toLowerCase();
+		System.out.println(s1lower);
+		
+	}
+
+}
