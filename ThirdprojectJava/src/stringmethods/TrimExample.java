aab69eebecb33983f88caa94cdbd886b80caee6c
Anjana111222 <karkianjana111@gmail.com>
2023-09-22 14:50:54 -0500

    Glab303.3.1 example 3 Trimexample

---
+++ ThirdprojectJava/src/stringmethods/TrimExample.java
@@ -0,0 +1,11 @@
+package stringmethods;
+
+public class TrimExample {
+	public static void main(String[] args) {
+		String s1 = "   hello   ";
+		System.out.println(s1+"How are you?");
+		System.out.println(s1.trim()+" How are you?");
+		
+	}
+
+}
