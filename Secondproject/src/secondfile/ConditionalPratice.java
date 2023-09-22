4435b341de8ceb842098208ed112f13ccdba8f9d
Anjana111222 <karkianjana111@gmail.com>
2023-09-22 13:50:13 -0500

    Conditional pratice 1 from module 303.4

---
+++ Secondproject/src/secondfile/ConditionalPratice.java
@@ -0,0 +1,16 @@
+package secondfile;
+
+public class ConditionalPratice {
+	public static void main (String[] args) {
+		int x = 50;
+		if (x < 10) {
+			System.out.println("Less than 10");
+		}else if ((x < 10) && (x > 20)){
+			System.out.println("Greater than or equal to 20");
+			}else {
+			System.out.println("Greater than or equal to 20");
+		}
+	}
+	
+
+}
