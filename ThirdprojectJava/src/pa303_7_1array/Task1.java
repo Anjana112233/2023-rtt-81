493a0c07c9b8b2bfdd766d9874c5243ca073d055
Anjana111222 <karkianjana111@gmail.com>
2023-09-26 15:10:48 -0500

    pratice assignment 303.7.1 Task 1

--- ThirdprojectJava/src/pa303_7_1array/Task1.java
+++ ThirdprojectJava/src/pa303_7_1array/Task1.java
@@ -1,5 +1,23 @@
 package pa303_7_1array;
 
 public class Task1 {
+	//Write a program that creates an array of integers with a length of 3. Assign the values 1, 2, and 3 to the indexes.
+	//Print out each array element.
+	public static void main(String[] args) {
+		//create an array of integers with length 3
+		int[] myArray = new int[3];
+		
+		//Assign values to the array elements
+		myArray[0] = 1;
+		myArray[1] = 2;
+		myArray[2] = 3;
+		
+		//print out each array element
+		System.out.println("Element at index 0: "+ myArray[0]);
+		System.out.println("Element at index 1: "+ myArray[1]);
+		System.out.println("Element at index 2: "+ myArray[2]);
+		
+	}
+
 
 }
