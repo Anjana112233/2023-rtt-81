06146d0e4bf6f4cab799061525445b975a0a2b7e
Anjana111222 <karkianjana111@gmail.com>
2023-09-27 08:12:23 -0500

    Podition of the last space homework

--- ThirdprojectJava/src/stringmethods/SubString.java
+++ ThirdprojectJava/src/stringmethods/SubString.java
@@ -28,6 +28,36 @@ public class SubString {
 
 		String lastname = name.substring(name.indexOf(" ") + 1);
 		System.out.println(lastname);
+		
+		// find the position of the first space in the string
+		//------------------------012345678901234567
+				String example = "This is an example";
+				
+				// find the position of the last space in the string
+				int lastSpaceIndex = example.lastIndexOf(" ");
+				
+				// check if a space was found
+				if (lastSpaceIndex != -1) {
+					System.out.println("Position of the last space : " + lastSpaceIndex);
+				}
+				else {
+					System.out.println("No space found in the spring");
+				}
+				
+				
+				// substring of the characters between the first and last space of the string
+				
+				
+				// do the same thing to find the substring of the first and last vowel of the string
+			
+				
+				// use the string.charAt function and use a for loop to loop over the entire string
+				// and print out each character with a new line after it.
+				// you need to use the string.length() function also
+				
+				// using only indexOf(" ") print the substring that contains the word "is"
+				// use 2 substrings to do this.
+				
 
 	}
 
