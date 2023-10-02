31ddcae5a91a056f1f877b234774e5ee06cd55ae
Anjana111222 <karkianjana111@gmail.com>
2023-10-02 12:25:38 -0500

    Sub of numbers between 1 and 100 pratice lab

---
+++ ThirdprojectJava/src/praticelabbeforesab/SumOfNumbers.java
@@ -0,0 +1,16 @@
+package praticelabbeforesab;
+
+public class SumOfNumbers {
+
+	public static void main(String[] args) {
+		int sumofNumbers = 0;
+		for (int i = 0; i <= 100; i++) {
+			sumofNumbers = sumofNumbers + i;
+			
+//calculate the sum of all numbers between 1 and 100.
+		}
+		System.out.println("The sume of all numbers between 1 to 100 is : " + sumofNumbers);
+
+	}
+
+}
