5ca34ce2ed0f1565454c6a3199c42d8b598f7f18
Anjana111222 <karkianjana111@gmail.com>
2023-10-27 14:00:19 -0500

    Started new junit project

--- database/src/main/java/org/perscholas/database/dao/OrderDetailDAO.java
+++ database/src/main/java/org/perscholas/database/dao/OrderDetailDAO.java
@@ -8,31 +8,34 @@ import org.perscholas.database.entity.Customer;
 import org.perscholas.database.entity.OrderDetail;
 
 public class OrderDetailDAO {
-	public OrderDetail findById(Integer orderId) {
+	public OrderDetail findByOrderIdAndProductId(Integer orderId, Integer productId) {
 		SessionFactory factory = new Configuration().configure().buildSessionFactory();
 		Session session = factory.openSession();
 
-		String hql = "FROM OrderDetail od WHERE od.id = :orderId"; // Example of HQL to get all records of user class
-        
-		System.out.println(hql);
-		
-		TypedQuery<OrderDetail> query = session.createQuery(hql, OrderDetail.class);
-		
-		query.setParameter("id", orderId);
+		String hql = "FROM OrderDetail od WHERE od.order.id = :orderId and od.product.id = :productId"; // Example of HQL to get all records of user class
 
-		OrderDetail result = query.getSingleResult();
-		return result;
+		TypedQuery<OrderDetail> query = session.createQuery(hql, OrderDetail.class);
+		query.setParameter("orderId", orderId);
+		query.setParameter("productId", productId);
+
+		try {
+			OrderDetail result = query.getSingleResult();
+			
+			return result;
+		} catch (NoResultException nre) {
+			return null;
+		}
 	}
+	
+	
 	public void save(OrderDetail orderDetail) {
 		SessionFactory factory = new Configuration().configure().buildSessionFactory();
 		Session session = factory.openSession();
-		//session.beginTransaction();
-		//session.save(save);
 
 		Transaction t = session.beginTransaction();
 		session.saveOrUpdate(orderDetail);
 		t.commit();
 		session.close();
-
 	}
+	
 }
